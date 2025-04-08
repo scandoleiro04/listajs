179a593d7f0f1906d1f3ca5b8cd70ed6f385094b
julia scandoleiro <j.scandoleiro@aluno.ifsp.edu.br>
2025-04-08 16:16:00 -0300

    exercicio 4

---
+++ Desktop/listajs/ex4.js
@@ -0,0 +1,8 @@
+function exercicio4() {
+    let nota1 = Number(prompt("Digite a primeira nota:"));
+    let nota2 = Number(prompt("Digite a segunda nota:"));
+    let nota3 = Number(prompt("Digite a terceira nota:"));
+   
+    let media = (nota1 + nota2 + nota3) / 3;
+    console.log("Média: " + media);
+}
