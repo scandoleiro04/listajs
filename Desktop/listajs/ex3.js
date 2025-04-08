e21cc6e81bf969ac6a6e837584364415cbb73f9e
julia scandoleiro <j.scandoleiro@aluno.ifsp.edu.br>
2025-04-08 16:15:30 -0300

    exercicio 3

---
+++ Desktop/listajs/ex3.js
@@ -0,0 +1,7 @@
+function exercicio3() {
+    let nome = prompt("Digite seu nome:");
+    let sobrenome = prompt("Digite seu sobrenome:");
+   
+    let nomeCompleto = nome + " " + sobrenome;
+    console.log("Nome completo: " + nomeCompleto);
+}
