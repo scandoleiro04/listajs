913e67ed074418cf45e2cb236ea8efc1268d071f
julia scandoleiro <j.scandoleiro@aluno.ifsp.edu.br>
2025-04-08 16:14:04 -0300

    exercicio 1

---
+++ Desktop/listajs/ex1.js
@@ -0,0 +1,9 @@
+function exercicio1() {
+    let nome = "julia"; 
+    let idade = 18; 
+    let isAdulto = true; 
+   
+    console.log(nome);
+    console.log(idade);
+    console.log(isAdulto);
+}
