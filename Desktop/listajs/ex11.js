108d9999c1e9ce67df88f6ad546c76eec9a8d023
julia scandoleiro <j.scandoleiro@aluno.ifsp.edu.br>
2025-04-08 16:23:04 -0300

    exercicio 11

---
+++ Desktop/listajs/ex11.js
@@ -0,0 +1,11 @@
+function exercicio11() {
+    let numero = Number(prompt("Digite um número:"));
+   
+    if (numero > 0) {
+        console.log("O número é positivo.");
+    } else if (numero < 0) {
+        console.log("O número é negativo.");
+    } else {
+        console.log("O número é zero.");
+    }
+}
