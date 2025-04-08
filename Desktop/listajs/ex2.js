52bba1ee9ab65236e2c3323185e2d010e0438924
julia scandoleiro <j.scandoleiro@aluno.ifsp.edu.br>
2025-04-08 16:14:57 -0300

    exercicio 2

---
+++ Desktop/listajs/ex2.js
@@ -0,0 +1,7 @@
+function exercicio2() {
+    let valor = prompt("Digite um valor:");
+    let numero = Number(valor);
+    let resultado = numero + 10;
+   
+    console.log("Resultado: " + resultado);
+}
