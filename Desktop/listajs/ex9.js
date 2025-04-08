e427ea81b3eecbf0ad8385adcbb366999385370f
julia scandoleiro <j.scandoleiro@aluno.ifsp.edu.br>
2025-04-08 16:21:41 -0300

    exercicio 9

---
+++ Desktop/listajs/ex9.js
@@ -0,0 +1,9 @@
+function exercicio9() {
+    let idade = Number(prompt("Qual a sua idade?"));
+   
+    if (idade >= 18) {
+        console.log("Você é maior de idade.");
+    } else {
+        console.log("Você é menor de idade.");
+    }
+}
