02f005373a147010480e9858b3726b2dcc6e788c
julia scandoleiro <j.scandoleiro@aluno.ifsp.edu.br>
2025-04-08 16:21:01 -0300

    exercicio 8

---
+++ Desktop/listajs/ex8.js
@@ -0,0 +1,7 @@
+function exercicio8() {
+    let resultado1 = Number("14") + 5;
+    let resultado2 = "14" + 5;
+   
+    console.log("Number('14') + 5: " + resultado1);
+    console.log('"14" + 5: ' + resultado2);
+}
