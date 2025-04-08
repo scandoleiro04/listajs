7c4de841f354dfc8bb9903024f4a138c52c1619f
julia scandoleiro <j.scandoleiro@aluno.ifsp.edu.br>
2025-04-08 16:17:33 -0300

    exercicio 5

---
+++ Desktop/listajs/ex5.js
@@ -0,0 +1,9 @@
+function exercicio5() {
+    let numero = Number(prompt("Digite um número:"));
+   
+    if (numero % 2 === 0) {
+        console.log("O número é par.");
+    } else {
+        console.log("O número é ímpar.");
+    }
+}
